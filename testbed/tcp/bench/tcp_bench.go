ae0a1cbe2d4bc3d1b75617bc43264087b05e0b69
gildarov <gildarov@ozon.ru>
2023-04-24 01:06:25 +0300

    finish writing tcp_bench

--- testbed/tcp/bench/tcp_bench.go
+++ testbed/tcp/bench/tcp_bench.go
@@ -3,44 +3,57 @@ package main
 import (
 	"log"
 	"net"
+	"syscall"
 	"time"
 )
 
+// #include <stdlib.h>
+import "C"
+
 func testConnect() {
-	localAddr := &net.TCPAddr{
-		Port: 45000,
+	dialer := net.Dialer{
+		Control: reusePort,
+		LocalAddr: &net.TCPAddr{
+			Port: 45000,
+		},
 	}
-	remoteAddr := &net.TCPAddr{
-		Port: 3000,
-	}
-	conn, err := net.DialTCP("tcp4", localAddr, remoteAddr)
+	conn, err := dialer.Dial("tcp4", ":3000")
 	if err != nil {
 		log.Fatal(err)
 	}
-	conn.Close()
+	err = conn.Close()
+	if err != nil {
+		log.Fatalf("close err: %s", err.Error())
+	}
 }
 
 func main() {
-	// 10 connections
+	// 100 connections
 	start := time.Now()
-	for i := 0; i < 1e4; i++ {
+	for i := 0; i < 1e2; i++ {
 		testConnect()
 	}
 	end := time.Now().Sub(start).Seconds()
 	log.Printf("10 conn: %f\n", end)
 
-	// 100 connections
+	// 1000 connections
 	start = time.Now()
-	for i := 0; i < 1e7; i++ {
+	for i := 0; i < 1e3; i++ {
 		testConnect()
 	}
 	end = time.Now().Sub(start).Seconds()
 	log.Printf("100 conn: %f\n", end)
 
-	// 1000 connections
-	for i := 0; i < 1e10; i++ {
+	// 10000 connections
+	for i := 0; i < 1e4; i++ {
 		testConnect()
 	}
 	end = time.Now().Sub(start).Seconds()
 	log.Printf("1000 conn: %f\n", end)
+}
+
+func reusePort(network, address string, conn syscall.RawConn) error {
+	return conn.Control(func(descriptor uintptr) {
+		syscall.SetsockoptInt(int(descriptor), syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1)
+	})
 }
--- testbed/tcp/server.go
+++ testbed/tcp/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"os"
@@ -9,10 +10,9 @@ import (
 )
 
 func main() {
-	addr := &net.TCPAddr{
-		Port: 3000,
-	}
-	server, err := net.ListenTCP("tcp4", addr)
+	config := &net.ListenConfig{Control: reusePort}
+
+	server, err := config.Listen(context.Background(), "tcp4", ":3000")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,10 +34,16 @@ func main() {
 
 	log.Println("listening on port 3000")
 	for {
-		_, err := server.AcceptTCP()
+		_, err := server.Accept()
 		if err != nil {
-			log.Println("err: %s", err.Error())
+			log.Printf("err: %s", err.Error())
 			return
 		}
 	}
+}
+
+func reusePort(network, address string, conn syscall.RawConn) error {
+	return conn.Control(func(descriptor uintptr) {
+		syscall.SetsockoptInt(int(descriptor), syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1)
+	})
 }
